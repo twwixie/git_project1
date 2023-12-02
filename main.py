6fb18c9fb7b9938292b3f6fe7975d5a0ef3d5a1a
twwixie <booch2019@mail.ru>
2023-12-02 19:12:08 +0300

    we need more commits

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def main():
     print('My first git program')
     print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+    print('mya')
 
 
 if __name__ == '__main__':
